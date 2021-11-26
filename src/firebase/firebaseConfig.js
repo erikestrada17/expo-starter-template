cea7405d7cc19695442df1a555dd0576a33c54c5
Erik Estrada <erikestradaf21@gmail.com>
2021-11-26 13:01:46 -0300

    Add environment variables in Expo

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -5,13 +5,13 @@ import 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyArwimaM0PrRde0x7C_CXMKCJGI0Ugigqc",
-    authDomain: "pruebafirebase-ee21.firebaseapp.com",
-    projectId: "pruebafirebase-ee21",
-    storageBucket: "pruebafirebase-ee21.appspot.com",
-    messagingSenderId: "433488097868",
-    appId: "1:433488097868:web:74fc1257dc5207406bfae9",
-    measurementId: "G-6HWHJGP09K"
+    apiKey: process.env.APP_APIKEY,
+    authDomain: process.env.APP_AUTHDOMAIN,
+    projectId: process.env.APP_PROJECTID,
+    storageBucket: process.env.APP_STORAGEBUCKET,
+    messagingSenderId: process.env.APP_MESSAGINGSENDERID,
+    appId: process.env.APP_APPID,
+    measurementId: process.env.APP_MEASUREMENTID
 };
 // Initialize Firebase
 const fb = firebase.initializeApp(firebaseConfig);
